bd4cf72e80a62a1590e63149f3fa5fbe35be2c5a
ChoiHeekyeong <64201163+ChoiHeekyeong@users.noreply.github.com>
2020-05-12 22:16:44 +0900

    Add files via upload

---
+++ day01/src/데이터/데이터연습.java
@@ -0,0 +1,12 @@
+package 데이터;
+
+public class 데이터연습 {
+
+	public static void main(String[] args) {
+		//comment(주석)
+		System.out.println("안녕하세요");
+		
+		
+	}
+
+}
---
+++ day01/src/데이터/데이터연습2.java
@@ -0,0 +1,11 @@
+package 데이터;
+
+public class 데이터연습2 {
+
+	public static void main(String[] args) {
+		//모니터에 출력해봅시다.
+		System.out.println("반갑습니다."); //sysout(syso) +컨트롤+스페이스바 누르면 편하다
+		System.out.println("마지막 시간입니다.");
+	}
+
+}
